c6efc9499636f512164d801c6e24470caf82abd9
Dishan Fernando <dmfernando91@gmail.com>
2026-02-10 07:30:20 +0530

    build change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const config = defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        options: resolve(__dirname, "src/options/index.html"),
+        popup: resolve(__dirname, "src/popup/index.html"),
+      },
+    },
+  },
+});
+
+console.log(config.plugins);
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default config;
